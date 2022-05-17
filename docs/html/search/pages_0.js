241e82df0b017e83285e8e77c9b4a2f9117575f9
smroccuzzo <santom.roccuzzo@gmail.com>
2022-05-17 10:41:48 +0200

    Updated documentation

--- docs/html/search/pages_0.js
+++ docs/html/search/pages_0.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['example_2d1',['example-1',['../example-1.html',1,'']]],
   ['example_2d2',['example-2',['../example-2.html',1,'']]],
-  ['example_2d3',['example-3',['../example-3.html',1,'']]]
+  ['example_2d3',['example-3',['../example-3.html',1,'']]],
+  ['example_2d4',['example-4',['../example-4.html',1,'']]]
 ];
