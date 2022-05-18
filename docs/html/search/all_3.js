87a812dc36e98b66cdec4bc8ba00ac6a77e03251
smroccuzzo <santom.roccuzzo@gmail.com>
2022-05-18 16:10:52 +0200

    fixed link to examples section in docs

--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -4,6 +4,6 @@ var searchData=
   ['example_2d2_1',['example-2',['../example-2.html',1,'examples']]],
   ['example_2d3_2',['example-3',['../example-3.html',1,'examples']]],
   ['example_2d4_3',['example-4',['../example-4.html',1,'examples']]],
-  ['examples_4',['Examples',['../examples.html',1,'']]],
+  ['examples_4',['Examples',['../examples.html',1,'index']]],
   ['extent_5',['extent',['../class_ultra_cold_1_1_vector.html#a255ce5c3acc5afceb2052d9eb3f338a3',1,'UltraCold::Vector']]]
 ];
--- docs/html/search/pages_1.js
+++ docs/html/search/pages_1.js
@@ -4,5 +4,5 @@ var searchData=
   ['example_2d2_1',['example-2',['../example-2.html',1,'examples']]],
   ['example_2d3_2',['example-3',['../example-3.html',1,'examples']]],
   ['example_2d4_3',['example-4',['../example-4.html',1,'examples']]],
-  ['examples_4',['Examples',['../examples.html',1,'']]]
+  ['examples_4',['Examples',['../examples.html',1,'index']]]
 ];
